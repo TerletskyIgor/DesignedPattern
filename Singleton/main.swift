4be615990ee923704055335fd22be93e61fd30fb
Igor Terletskyi <iterletskyi@teamworkcommerce.com>
2020-02-23 15:26:03 +0200

    Initial Commit

---
+++ Singleton/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Singleton
+//
+//  Created by Igor Terletskyi on 23.02.2020.
+//  Copyright © 2020 Igor Terletskyi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
